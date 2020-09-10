22f800d77b092d354e3636c40aa01fc9b18841a8
JuanSessions <juanperusin@yahoo.es>
2020-09-10 17:30:43 +0200

    deploy commands corrections etc

--- src/products.js
+++ src/products.js
@@ -4,28 +4,28 @@ const products = [{
     "brand": "Treck",
     "size": "medium",
     "type": "men",
-    "img": "/img/serious-mt-cataract-fs-275-black-1.jpg"
+    "img": "./img/serious-mt-cataract-fs-275-black-1.jpg"
 }, {
     "id": 2,
     "name": "Urban",
     "brand": "Giant",
     "size": "big",
     "type": "children",
-    img: "/img/ortler-summerfield-7-speed-women-white-2.jpg"
+    img: "./img/ortler-summerfield-7-speed-women-white-2.jpg"
 }, {
     "id": 3,
     "name": "City-Mountain Bike",
     "brand": "Raleigh",
     "size": "medium",
     "type": "women",
-    img: "/img/fixie-inc-blackheath-petrol-red-2.jpg"
+    img: "./img/fixie-inc-blackheath-petrol-red-2.jpg"
 }, {
     "id": 4,
     "name": "Speed",
     "brand": "Bianchi",
     "size": "small",
     "type": "men",
-    img: "/img/trek-domane-al-2-trek-black-carbon-smoke-1.jpg"
+    img: "./img/trek-domane-al-2-trek-black-carbon-smoke-1.jpg"
 }]
 
 export default products;
