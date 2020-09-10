38ec330d6241128e546034ec54960ae370fd9b2a
JuanSessions <juanperusin@yahoo.es>
2020-09-10 17:22:35 +0200

    Updates

---
+++ precache-manifest.6f9e1641278742a6e84993fda15b3b1b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbacd9614f073e21afde6632e1723722",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18a29ad02df8ada61e61",
+    "url": "./static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "5d5540ea76aa4155858d",
+    "url": "./static/css/main.ada1508d.chunk.css"
+  },
+  {
+    "revision": "18a29ad02df8ada61e61",
+    "url": "./static/js/2.2b3bb863.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.2b3bb863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5540ea76aa4155858d",
+    "url": "./static/js/main.dbed80f7.chunk.js"
+  },
+  {
+    "revision": "c49f88d979136dfb3381",
+    "url": "./static/js/runtime-main.62e1864c.js"
+  },
+  {
+    "revision": "d665687562d3f4c8c5a19519a9b5bc6d",
+    "url": "./static/media/bike-1836934_1920.d6656875.jpg"
+  },
+  {
+    "revision": "0aa85daa5e6a1c193b42a9007f51e9e1",
+    "url": "./static/media/gear-2291916_1920.0aa85daa.jpg"
+  },
+  {
+    "revision": "ccd09bf305f84257eb49559308d61cf0",
+    "url": "./static/media/logo192.ccd09bf3.png"
+  }
+]);
